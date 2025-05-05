7ab3b22218c50b027a451b0d2e21e3a012aadb1e
jpaxcoop <jasonpaxtoncooper@gmail.com>
2025-05-05 08:08:19 -0400

    Use Image components instead of img elements

--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function HomeBackground() {
@@ -5,36 +6,84 @@ export default function HomeBackground() {
     <div className="absolute inset-0 z-[-1]">
         <div className="absolute inset-0 bg-gradient-to-tr bg-gradient-to-r from-blue-200 via-blue-200 to-pink-200" />
 
-        <div className="absolute top-[-10vh] left-[70vw] z-0 animate-cloud-initial" style={{ animationDuration: '65s' }}>
-            <img src="/cloud.png" alt="" className="h-[40vh]" />
+        <div className="absolute top-[-18vh] left-[50vw] z-0 animate-cloud-initial" style={{ animationDuration: '65s' }}>
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "48vh", width: "auto" }}
+            />
         </div>
 
         <div className="absolute top-[-16vh] left-[10vw] z-0 animate-cloud-initial" style={{ animationDuration: '65s' }}>
-            <img src="/cloud.png" alt="" className="h-[48vh]" />
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "48vh", width: "auto" }}
+            />
         </div>
 
         <div className="absolute top-[-20vh] left-[-35vw] z-0 animate-cloud-loop" style={{ animationDuration: '105s' }}>
-            <img src="/cloud.png" alt="" className="h-[44vh]" />
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "44vh", width: "auto" }}
+            />
         </div>
 
         <div className="absolute top-[-18vh] left-[-35vw] z-0 animate-cloud-loop" style={{ animationDuration: '95s', animationDelay: '5s' }}>
-            <img src="/cloud.png" alt="" className="h-[40vh]" />
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "40vh", width: "auto" }}
+            />
         </div>
 
         <div className="absolute top-[-15vh] left-[-35vw] z-0 animate-cloud-loop" style={{ animationDuration: '90s', animationDelay: '15s' }}>
-            <img src="/cloud.png" alt="" className="h-[36vh]" />
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "36vh", width: "auto" }}
+            />
         </div>
 
         <div className="absolute top-[-12vh] left-[-45vw] z-0 animate-cloud-loop" style={{ animationDuration: '100s', animationDelay: '30s' }}>
-            <img src="/cloud.png" alt="" className="h-[40vh]" />
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "40vh", width: "auto" }}
+            />
         </div>
 
         <div className="absolute top-[-16vh] left-[-55vw] z-0 animate-cloud-loop" style={{ animationDuration: '80s', animationDelay: '40s' }}>
-            <img src="/cloud.png" alt="" className="h-[48vh]" />
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "48vh", width: "auto" }}
+            />
         </div>
 
         <div className="absolute top-[-12vh] left-[-55vw] z-0 animate-cloud-loop" style={{ animationDuration: '95s', animationDelay: '50s' }}>
-            <img src="/cloud.png" alt="" className="h-[44vh]" />
+            <Image
+                src="/cloud.png"
+                alt=""
+                height={0}
+                width={100}
+                style={{ height: "44vh", width: "auto" }}
+            />
         </div>
 
         <div
